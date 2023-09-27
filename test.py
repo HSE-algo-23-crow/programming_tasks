ddfe11a8980423a1f668a3337f7df2f10d6afc49
Myron Butakov <mabutakov_1@edu.hse.ru>
2023-09-27 21:00:03 +0500

    Реализована функция "Hello World!". В соответствии с заданием в тесте происходит проверка вывода (World с заглавной буквы!)

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from main import hello
 
 class TestHello(unittest.TestCase):
     def test_hello(self):
-        self.assertEqual(hello(), 'Hello world!')
+        self.assertEqual(hello(), 'Hello World!')
 
 
 if __name__ == '__main__':
