a7c464526c836b2946fb18956033330fa7feb21d
Dmitry Baluev <nft.rebel.main@gmail.com>
2023-09-27 23:27:11 +0500

    Created rabbits func.

--- main.py
+++ main.py
@@ -7,7 +7,16 @@ def fibonacci_rec(n: int) -> int:
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
-    pass
+    if type(n) != int:
+        raise Exception('Параметр n не является целом числом')
+    if n < 1:
+        raise Exception('Параметр n меньше 1')
+
+    if n == 1:
+        return 0
+    elif n in (2, 3):
+        return 1
+    return fibonacci_rec(n - 1) + fibonacci_rec(n - 2)
 
 
 def fibonacci_iter(n: int) -> int:
@@ -16,10 +25,23 @@ def fibonacci_iter(n: int) -> int:
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
-    pass
+    if type(n) != int:
+        raise Exception('Параметр n не является целым числом')
+    if n < 1:
+        raise Exception('Параметр n меньше 1')
+
+    fib1 = fib2 = 1
+    n -= 3
+
+    while n > 0:
+        fib1, fib2 = fib2, fib1 + fib2
+        n -= 1
+
+    return fib2
 
 
 def rabbits(month: int, lifetime: int) -> int:
+
     """Возвращает количество пар кроликов в популяции на заданный месяц.
     В начальный момент времени имеется одна пара кроликов. Начиная со второго
     месяца после рождения кролики производят новую пару кроликов каждый месяц.
@@ -29,7 +51,18 @@ def rabbits(month: int, lifetime: int) -> int:
     :param lifetime: продолжительность жизни кролика, не менее 2 месяцев
     :return: количество пар кроликов
     """
-    pass
+    if month <= 0:
+        return 0
+    elif month == 1:
+        return 1
+    else:
+        pairs = [1, 1]
+        for i in range(2, month):
+            new_pairs = pairs[i-1] + pairs[i-2]
+            if i >= lifetime:
+                new_pairs -= pairs[i-lifetime]
+            pairs.append(new_pairs)
+        return pairs[-1]
 
 
 def main():
