04bf1f2c260965d7973ce170f6c36265ef12945b
Gucol1 <gucol2004@mail.ru>
2023-10-01 16:48:42 +0500

    citrine task 2+ completed

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ MATRIX = 'matrix'
 DET = 'det'
 
 
-def get_random_matrix_and_det(order):
+def get_random_matrix_and_det(order) -> {}:
     """Генерирует случайную квадратную целочисленную матрицу с заранее
     известным значением определителя.
 
@@ -17,18 +17,65 @@ def get_random_matrix_and_det(order):
     меньше 1
     :return: словарь с ключами matrix, det
     """
-    pass
+
+    validate_order_raises_exception(order)
+    return create_random_matrix(order)
+
+
+def create_random_matrix(order) -> {}:
+    """
+    Функция, которая создаёт "случайную матрицу"
+    :param order: порядок целочисленной квадратной матрицы
+    :return: возвращает словарь с матрицей и её определителем
+    """
+    matrix_and_det = {'matrix': [], 'det': 1}
+
+    for row_index in range(0, order):
+        matrix_row = []
+
+        for column_index in range(0, order):
+            if column_index < row_index:
+                matrix_row.append(0)
+            else:
+                matrix_row.append(rnd.randint(-10, 10))
+
+            if column_index == row_index:
+                matrix_and_det['det'] *= matrix_row[column_index]
+
+        matrix_and_det['matrix'].append(matrix_row)
+
+    for row_index in range(1, order):
+        k = random.randint(2, 5)
+        for column_index in range(0, order):
+            matrix_and_det['matrix'][row_index][column_index] += matrix_and_det['matrix'][row_index-1][column_index]*k
+    return matrix_and_det
+
+
+def validate_order_raises_exception(order):
+    """
+    Функция, которая проверяет валидность введённых данных
+    :param order: порядок целочисленной квадратной матрицы
+    :raise Exception: если порядок матрицы неверного типа или меньше единицы
+    """
+    if type(order) != int:
+        raise TypeError("Неверный тип данных для введённого значения порядка!")
+    elif order < 1:
+        raise ValueError("Порядок матрицы не может быть меньше единицы!")
 
 
 def main():
+    """
+    Основное тело программы
+    :return:
+    """
     n = 10
     print('Генерация матрицы порядка 10')
-    gen_result = get_random_matrix_and_det(n)
-    [print(row) for row in gen_result[MATRIX]]
-    print('\nОпределитель сгенерированной матрицы равен', gen_result[DET])
+    matrix_and_det = get_random_matrix_and_det(n)
+    [print(row) for row in matrix_and_det[MATRIX]]
+    print('\nОпределитель сгенерированной матрицы равен', matrix_and_det[DET])
 
     print('\nОпределитель, рассчитанный numpy, равен',
-          round(np.linalg.det(np.array(gen_result[MATRIX]))))
+          round(np.linalg.det(np.array(matrix_and_det['matrix']))))
 
 
 if __name__ == '__main__':
