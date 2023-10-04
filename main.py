45022edc02f301bf5e23b1c6ca06e37af28fdd8d
Dmitry Baluev <nft.rebel.main@gmail.com>
2023-10-04 23:20:03 +0500

    Создана функция для проверки квадратной целочисленной матрицы. Создана функция для вычисления минора.

--- main.py
+++ main.py
@@ -1,12 +1,58 @@
-def calculate_determinant(matrix: [[int]]) -> int:
+import numpy
+
+
+def calculate_determinant(_matrix: [[int]]) -> int:
     """Вычисляет определитель целочисленной квадратной матрицы
 
-    :param matrix: целочисленная квадратная матрица
-    :raise Exception: если значение параметра не является целочисленной
+    :param _matrix: Целочисленная квадратная матрица
+    :raise Exception: Если значение параметра не является целочисленной
     квадратной матрицей
-    :return: значение определителя
+    :return: Значение определителя
     """
+    square_matrix_check(_matrix)
+
+    if len(_matrix) == 1:
+        return _matrix[0][0]
+
+    row_idx = choose_optimal_row_idx(_matrix)
+
+    det = 0
+    for column_idx in range(len(_matrix)):
+        if _matrix[row_idx][column_idx] == 0:
+            continue
+        det += _matrix[row_idx][column_idx] * (-1) ** (row_idx + column_idx) * calculate_minor(_matrix, row_idx,
+                                                                                               column_idx)
+
+    return det
+
+
+def calculate_minor(matrix, row_idx, column_idx):
+    minor = numpy.delete(numpy.delete(matrix, row_idx, axis=0), column_idx, axis=1)
+    return minor
+
+
+def choose_optimal_row_idx():
     pass
+
+
+def square_matrix_check(_matrix):
+    """ Вычисляет минор целочисленной квадратной матрицы
+
+    :param _matrix: Целочисленная квадратная матрица?
+    :raise Exception: Если значение параметра не является целочисленной
+    квадратной матрицей
+    :return:
+    """
+    if type(_matrix) != list:
+        raise Exception("Введенное значение не является списком!")
+    for row in _matrix:
+        if type(row) != list:
+            raise Exception("Введенное значение не является вложенным списком!")
+        if len(row) != len(_matrix):
+            raise Exception("Введенное значение не является квадратной матрицей!")
+        for ind in row:
+            if type(ind) != int:
+                raise Exception("Значения в квадратной матрице не является целочисленными!")
 
 
 def main():
