a5ed9f857b1edadd919d0782ce85c47d531297f3
Dmitry Baluev <nft.rebel.main@gmail.com>
2023-10-05 13:22:11 +0500

    Задание полностью написано. Юнит тест пройден.

--- main.py
+++ main.py
@@ -3,26 +3,52 @@ import random
 import numpy as np
 import random as rnd
 
-
 MATRIX = 'matrix'
 DET = 'det'
 
 
 def get_random_matrix_and_det(order):
     """Генерирует случайную квадратную целочисленную матрицу с заранее
-    известным значением определителя.
-
-    :param order: порядок матрицы
-    :raise Exception: если порядок матрицы не является целым числом и порядок
+    известным значением определителя. Благодаря алгебраическим свойствам матрицы, мы можем использовать
+    сложение случайных строк данной квадратной матрицы для получения случайных значений
+    элементов без изменения определителя матрицы. Домножение первой строки матрицы на необходимое значение определителя
+    позволяет получить матрицу с рандомными значениями матрицы и необходимым определителем.
+    :param order: Порядок матрицы
+    :raise Exception: Если порядок матрицы не является целым числом и порядок
     меньше 1
-    :return: словарь с ключами matrix, det
+    :return: Словарь с ключами matrix, det
     """
-    pass
+    if type(order) != int or order < 1:
+        raise ValueError("Порядок должен быть натуральным целочисленным числом!")
+
+    if order == 1:
+        det = random.randint(1, 100)
+        matrix = np.array([[det, ], ])
+        return {MATRIX: matrix, DET: det}
+
+    matrix = np.zeros((order, order))
+
+    for i in range(order):
+        matrix[i, i] = 1  # Получение единичной матрицы
+
+    for i in range(order * rnd.randint(3, 8)):
+        row_indx = random.randint(0, order - 1)
+        col_indx = random.randint(0, order - 1)  # Получение случайных индексов строк матрицы для их сложения
+
+        while row_indx == col_indx:
+            col_indx = random.randint(0, order - 1) # Исключение сложения строки с собой же
+
+        matrix[row_indx] += matrix[col_indx] * rnd.randint(1, 5)
+
+    det = rnd.randint(1, 10)
+    matrix[0] *= det
+
+    return {MATRIX: matrix, DET: det}
 
 
 def main():
-    n = 10
-    print('Генерация матрицы порядка 10')
+    n = 3
+    print('Генерация матрицы порядка 3')
     gen_result = get_random_matrix_and_det(n)
     [print(row) for row in gen_result[MATRIX]]
     print('\nОпределитель сгенерированной матрицы равен', gen_result[DET])
