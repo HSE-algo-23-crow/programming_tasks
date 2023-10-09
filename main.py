4078a583ee19e0da64399b7fee2ef6fee82286d2
Myron Butakov <mabutakov_1@edu.hse.ru>
2023-10-09 20:46:56 +0500

    Все функции, за исключением main() прокомментированы. Рекурсивный метод вычисления НОД разбили на две функции - в первой проверка условий входных данных, во второй - вычисление, чтобы не повторять проверку в рекурсии. Также для вычисления НОД итеративным способом была создана вспомогательная функция gcd_helper, в ней находится код, который должен выполняться в начале медленной и быстрой итеративных реализаций. Для вычисления НОД и НОК были созданы функции (_validate) проверки входных данных на выполнение необходимых условий, о которых говорили на семинаре. Тесты успешно выполняются.

--- main.py
+++ main.py
@@ -2,16 +2,35 @@ import time
 
 
 def gcd_recursive(a: int, b: int) -> int:
-    """Вычисляет наибольший общий делитель двух целых чисел.
-    Рекурсивная реализация
+    """Функция-обертка включает в себя проверку чисел на выполнение необходимых условий и вызов функции-вычислителя
 
     :param a: целое число a
     :param b: целое число b
     :raise Exception: если a или b не являются целыми числами или
     они оба равны нулю
-    :return: значение наибольшего общего делителя
+    :return: значение наибольшего общего делителя (через функцию-вычислитель)
     """
-    pass
+    gcd_validate(a, b)
+    a = abs(a)
+    b = abs(b)
+
+    return gcd_recursive_computation(a, b)
+
+
+def gcd_recursive_computation(a: int, b: int) -> int:
+    """Функция-вычислитель - вычисляет наибольший общий делитель двух целых чисел.
+        Рекурсивная реализация
+
+        :param a: целое число a (a не равно 0)
+        :param b: целое число b (b не равно 0)
+        :return: значение наибольшего общего делителя
+        """
+    if b == 0:
+        return a
+    if b > a:
+        a, b = b, a
+
+    return gcd_recursive_computation(a - b, b)
 
 
 def gcd_iterative_slow(a: int, b: int) -> int:
@@ -24,7 +43,13 @@ def gcd_iterative_slow(a: int, b: int) -> int:
     они оба равны нулю
     :return: значение наибольшего общего делителя
     """
-    pass
+    a, b = gcd_helper(a, b)
+
+    while b != 0:
+        a, b = a - b, b
+        if b > a:
+            a, b = b, a
+    return a
 
 
 def gcd_iterative_fast(a: int, b: int) -> int:
@@ -37,7 +62,49 @@ def gcd_iterative_fast(a: int, b: int) -> int:
     они оба равны нулю
     :return: значение наибольшего общего делителя
     """
-    pass
+    a, b = gcd_helper(a, b)
+
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+
+def gcd_validate(a: int, b: int):
+    """Проверяет выполнения необходимых условий для вычисления НОД чисел a и b
+
+    :param a: целое число a
+    :param b: целое число b
+    :raise Exception: если a или b не являются целыми числами или
+    они оба равны нулю
+    """
+    if type(a) != int:
+        raise Exception("Значение параметра a не является целым числом")
+
+    if type(b) != int:
+        raise Exception("Значение параметра b не является целым числом")
+
+    if a == 0 and b == 0:
+        raise Exception("Значения параметров a и b равны нулю")
+
+
+def gcd_helper(a: int, b: int):
+    """Вспомогательная функция для вычисления НОД с помощью итеративного метода, для того чтобы не писать одинаковые
+     куски кода в медленной и быстрой версиях.
+     Функция проверяет числа a и b на выполнение необходимых условий, берет модули от чисел (не влияет на вычисление
+     НОД) и приводит числа к виду, что a - большее из двух чисел, b - соответственно меньшее
+
+    :param a:
+    :param b:
+    :return:
+    """
+    gcd_validate(a, b)
+    a = abs(a)
+    b = abs(b)
+
+    if b > a:
+        a, b = b, a
+
+    return a, b
 
 
 def lcm(a: int, b: int) -> int:
@@ -49,7 +116,23 @@ def lcm(a: int, b: int) -> int:
     они равны нулю
     :return: значение наименьшего общего кратного
     """
-    pass
+    lcm_validate(a, b)
+    return int(a * b / gcd_iterative_fast(a, b))
+
+
+def lcm_validate(a: int, b: int):
+    """Проверяет выполнения необходимых условий для вычисления НОК чисел a и b
+
+    :param a: натуральное число a
+    :param b: натуральное число b
+    :raise Exception: если a или b не являются целыми числами или
+    они оба равны нулю
+    """
+    if type(a) != int or a < 1:
+        raise Exception("Значение параметра a не является натуральным положительным числом")
+
+    if type(b) != int or b < 1:
+        raise Exception("Значение параметра b не является натуральным положительным числом")
 
 
 def main():
