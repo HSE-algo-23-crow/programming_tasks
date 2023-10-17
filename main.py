6a0d81b909a02d6c285accc236a08ad95d381c77
Myron Butakov <mabutakov_1@edu.hse.ru>
2023-10-17 21:02:17 +0500

    Реализованы функции generate_permutations и get_permutations_recursive. Функции прокомментированы. Учетны проверки входных данных. Тесты успешно выполняются.

--- main.py
+++ main.py
@@ -2,13 +2,44 @@ from typing import Any
 
 
 def generate_permutations(items: frozenset[Any]) -> list[Any]:
-    """Генерирует все варианты перестановок элементов указанного множества
+    """Функция-обертка проверяет пар. items на принадлежность типу frozenset, а также возвращает пустое множество, если
+    в items нет элементов для генерации перестановок
+
     :param items: неизменяемое множество элементов
     :raise TypeError: если параметр items не является неизменяемым множеством
-    :return: список перестановок, где каждая перестановка список элементов
+    :return: список перестановок, где каждая перестановка список элементов (через рекурсивную функцию)
     множества
     """
-    pass
+
+    if type(items) != frozenset:
+        raise TypeError("Параметр items не является неизменяемым множеством")
+
+    if len(items) == 0:
+        return []
+
+    return get_permutations_recursive(set([item for item in items]))
+
+
+def get_permutations_recursive(items):
+    """Рекурсивная функция для генерации всех перестановок из элементов items типа frozenset
+
+    :param items: неизменяемое множество элементов
+    :return: список перестановок, где каждая перестановка список элементов
+    """
+
+    if len(items) == 1:
+        return [[items.pop()]]
+
+    current_elem = items.pop()
+    permutations = get_permutations_recursive(items)
+    output = []
+
+    for permute in permutations:
+        for i in range(len(permute) + 1):
+            new_permutation = permute[:i] + [current_elem] + permute[i:]
+            output.append(new_permutation)
+
+    return output
 
 
 def main():
