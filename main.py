3a2904a1659334cab3a2ea618d314803efeed97d
Dmitry Baluev <nft.rebel.main@gmail.com>
2023-10-19 02:23:10 +0500

    Задание полностью исправлено. Алгоритм изменен на итеративный вариант, который основан на лексикографической перестановке. Написать еще один уникальный рекурсивный алгоритм не можем за неимением идей. Юнит тест пройден.

--- main.py
+++ main.py
@@ -8,28 +8,47 @@ def generate_permutations(items: frozenset[Any]) -> list[Any]:
     :return: список перестановок, где каждая перестановка список элементов
     множества
     """
-
     if type(items) != frozenset:
         raise TypeError("Параметр items не является неизменяемым множеством")
 
     if len(items) == 0:
-        return []
+        return list()
 
-    if len(items) == 1:
-        return [list(items)]
+    def next_permutation(arr):
+        n = len(arr)
+        i = n - 2
 
-    permutations = []
-    for element in items:
-        remaining_elements = items - frozenset([element])
-        sub_permutations = generate_permutations(remaining_elements)
-        for sub_permutation in sub_permutations:
-            permutations.append([element] + list(sub_permutation))
+        while i >= 0 and arr[i] >= arr[i + 1]:
+            i -= 1
+
+        if i == -1:
+            return None
+
+        j = n - 1
+
+        while arr[j] <= arr[i]:
+            j -= 1
+
+        arr[i], arr[j] = arr[j], arr[i]
+
+        arr[i + 1:] = arr[i + 1:][::-1]
+
+        return arr
+
+    elements_list = list(items)
+    elements_list.sort()
+    permutations = [elements_list[:]]
+
+    next_perm = next_permutation(elements_list)
+    while next_perm:
+        permutations.append(next_perm.copy())
+        next_perm = next_permutation(next_perm)
 
     return permutations
 
 
 def main():
-    items = frozenset([])
+    items = frozenset([1, 2, 3])
     print(generate_permutations(items))
 
 
