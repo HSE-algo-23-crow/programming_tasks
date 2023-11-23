79359cd032c4b299011b746361de5a880fc2204a
rmzvch <remuzevich159@gmail.com>
2023-11-23 10:07:23 +0500

    Realization of function calculate_determinant

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
-def calculate_determinant(matrix: [[int]]) -> int:
-    """Вычисляет определитель целочисленной квадратной матрицы
+def calculate_determinant(matrix):
+    # Проверяем, является ли матрица квадратной
+    if not all(len(row) == len(matrix) for row in matrix):
+        raise ValueError("Матрица не является квадратной")
 
-    :param matrix: целочисленная квадратная матрица
-    :raise Exception: если значение параметра не является целочисленной
-    квадратной матрицей
-    :return: значение определителя
-    """
-    pass
+    # Базовый случай: если матрица 1x1, то ее определитель равен единственному элементу
+    if len(matrix) == 1:
+        return matrix[0][0]
 
+    determinant = 0
+    for i in range(len(matrix)):
+        # Вычисляем минор (удаляем i-ю строку и 0-й столбец)
+        minor = [row[1:] for row in (matrix[:i] + matrix[i + 1:])]
 
-def main():
-    matrix = [[1, 2],
-              [3, 4]]
-    print('Матрица')
-    for row in matrix:
-        print(row)
+        # Вычисляем знак, который зависит от четности суммы индексов строки и столбца
+        sign = (-1) ** i
 
-    print(f'Определитель матрицы равен {calculate_determinant(matrix)}')
+        # Рекурсивно вычисляем определитель
+        determinant += sign * matrix[i][0] * calculate_determinant(minor)
 
+    return determinant
 
-if __name__ == '__main__':
-    main()
