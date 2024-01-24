7de54d8de22dec4f161c7bd5396d1b6f1545085c
rmzvch <remuzevich159@gmail.com>
2024-01-24 16:10:23 +0500

    Realization

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 PARAM_ERR_MSG = 'Таблица не является прямоугольной матрицей со значениями 0 1'
 
-
 def get_path_count(allow_table: list[list[int]]) -> int:
     """Возвращает количество допустимых путей в таблице из правого верхнего угла
     в левый нижний.
@@ -13,7 +12,39 @@ def get_path_count(allow_table: list[list[int]]) -> int:
     со значениями 0 1.
     :return: количество путей.
     """
-    pass
+
+    err = is_error(allow_table)
+    if (err): return err
+    return get_path_count_table(allow_table)
+
+
+def is_error(price_table):
+    # 1) табица не None 2) табица не пустая 3) первая строка табицы не пустая
+    if price_table == None or len(price_table) == 0 or len(price_table[0]) == 0: raise ValueError(PARAM_ERR_MSG)
+
+    for row in price_table:
+        # у всех строк одинаковая длина
+        if len(row) != len(price_table[0]): raise ValueError(PARAM_ERR_MSG)
+        for item in row:
+            # число либо 0, либо 1
+            if item not in [0, 1]: raise ValueError(PARAM_ERR_MSG)
+
+
+def get_path_count_table(price_table):
+    paths_table = [[0] * len(price_table[0]) for _ in range(len(price_table))]
+    # вставляем левый верхний элемент в копию таблицы
+    paths_table[0][0] = price_table[0][0]
+
+    for i in range(len(paths_table)):
+        for j in range(len(paths_table[0])):
+            # иначе остаётся нолик и мы ничего не делаем
+            if price_table[i][j] != 0:
+                # для ненулевых элментов добавляем то элементы сверху или слева
+                if i > 0: paths_table[i][j] += paths_table[i - 1][j]
+                if j > 0: paths_table[i][j] += paths_table[i][j - 1]
+
+    # возвращаем последний элемент копии таблицы
+    return paths_table[-1][-1]
 
 
 def main():
