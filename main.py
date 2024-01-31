fc916e2e7f13f5c9e33861a6e991c1b9d01eacf2
ImRedTea <avbantikov@edu.hse.ru>
2024-01-31 22:48:17 +0500

    halfway done

--- main.py
+++ main.py
@@ -29,13 +29,69 @@ def get_invest_distributions(profit_matrix: list[list[int]]) -> \
     distributions - списком со всеми вариантами распределения инвестиций между
     проектами, обеспечивающими максимальную прибыль.
     """
-    pass
+    validate_profit_matrix(profit_matrix)
+
+    n = len(profit_matrix[0])  # number of projects
+    m = len(profit_matrix)  # number of investment levels
+
+    # Initialize the dp and paths lists
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    paths = [[[[] for _ in range(n + 1)] for _ in range(m + 1)] for _ in range(n + 1)]
+
+    # Fill the dp and paths lists
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            for k in range(j + 1):
+                if k > 0:
+                    new_profit = profit_matrix[k - 1][i - 1] + dp[i - 1][j - k]
+                    if new_profit > dp[i][j]:
+                        dp[i][j] = new_profit
+                        paths[i][j] = [path + [k] for path in paths[i - 1][j - k]]
+                    elif new_profit == dp[i][j]:
+                        paths[i][j] += [path + [k] for path in paths[i - 1][j - k]]
+                else:
+                    #add zero investment to paths and dp
+                    paths[i][j] = [[0] + path for path in paths[i - 1][j]]
+                    dp[i][j] = dp[i - 1][j]
+            print(paths[1::])
+
+    #remove duplicates
+    paths[n][m] = list(set(tuple(path) for path in paths[n][m]))
+
+    i = 0
+    while i < len(paths[n][m]):
+        if sum(paths[n][m][i]) != m or len(paths[n][m][i]) != n:
+            paths[n][m].pop(i)
+        else:
+            i+=1
+
+    # Return the maximum profit and the corresponding distributions of investments
+    return {PROFIT: dp[n][m], DISTRIBUTIONS: paths[n][m]}
+
+
+def validate_profit_matrix(profit_matrix):
+    if not isinstance(profit_matrix, list) or not all(isinstance(row, list) and all(isinstance(value, int) for value in row) for row in profit_matrix):
+        raise ValueError(PARAM_ERR_MSG)
+    for column in range(1, len(profit_matrix)):
+        if len(profit_matrix[column]) != len(profit_matrix[0]):
+            raise ValueError(PARAM_ERR_MSG)
+    for column in range(len(profit_matrix) - 1):
+        for line in range(len(profit_matrix[0])):
+            if profit_matrix[column][line] > profit_matrix[column + 1][line]:
+                raise ProfitValueError(DECR_PROFIT_ERR_MSG, line, column + 1)
+
+    for column in range(len(profit_matrix)):
+        for line in range(len(profit_matrix[0])):
+            if profit_matrix[column][line] < 0:
+                raise ProfitValueError(NEG_PROFIT_ERR_MSG, line, column)
 
 
 def main():
-    profit_matrix = [[1, 1, 1],
-                     [2, 2, 2],
-                     [3, 3, 3]]
+    profit_matrix = [[5, 7, 2, 10],
+                  [9, 8, 4, 15],
+                  [11, 10, 5, 16],
+                  [12, 12, 8, 17],
+                  [14, 15, 9, 18]]
     print(get_invest_distributions(profit_matrix))
 
 
