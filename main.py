7a4f9ff9dc573b9bab522e7376c76d1829f0a77b
Дарья <dvchernova_1@edu.hse.ru>
2024-02-06 18:30:12 +0500

    Для решения данного задания необходимо было реализовать функцию get_invest_distributionпринимает таблицу прибыли profit_matrix от проектов в зависимости от уровня инвестиций. Функция проверяет входные данные на корректность и рассчитывает максимально возможную прибыль и способ распределение инвестиций между проектами.

--- main.py
+++ main.py
@@ -28,10 +28,24 @@ def get_invest_distribution(profit_matrix: list[list[int]]) -> \
     profit - максимально возможная прибыль от инвестиций,
     distribution - распределение инвестиций между проектами.
     """
-
-
-    if not all(isinstance(row, list) for row in profit_matrix) or not profit_matrix:
+    if (not profit_matrix) or (not isinstance(profit_matrix,list) or (profit_matrix is None) or (profit_matrix == []) or (profit_matrix == [[]])):
         raise ValueError(PARAM_ERR_MSG)
+
+    first_row_length = len(profit_matrix[0])
+    for row_index, row in enumerate(profit_matrix):
+        if len(row) != first_row_length:
+            raise ValueError(PARAM_ERR_MSG)
+
+        for item in row:
+            if not isinstance(item, (int, float)):
+                raise ValueError(PARAM_ERR_MSG)
+
+        for col_index, item in enumerate(row):
+            if item < 0:
+                raise ProfitValueError(NEG_PROFIT_ERR_MSG, col_index, row_index)
+            if row_index > 0 and profit_matrix[row_index - 1][col_index] > item:
+                raise ProfitValueError(DECR_PROFIT_ERR_MSG, col_index, row_index)
+
 
     num_projects = len(profit_matrix[0])
     num_levels = len(profit_matrix)
@@ -39,20 +53,31 @@ def get_invest_distribution(profit_matrix: list[list[int]]) -> \
     if num_projects == 0 or num_levels == 0:
         return {PROFIT: 0, DISTRIBUTION: [0] * num_projects}
 
-    max_profit = 0
+
+    for project_index in range(num_projects):
+        for level_index in range(num_levels):
+            if profit_matrix[level_index][project_index] < 0:
+                raise ProfitValueError(NEG_PROFIT_ERR_MSG, level_index, project_index)
+            if level_index > 0 and profit_matrix[level_index][project_index] < profit_matrix[level_index - 1][project_index]:
+                raise ProfitValueError(DECR_PROFIT_ERR_MSG, level_index, project_index)
+    max_profit_table = [[0]*(num_projects+1) for _ in range(num_levels+1)]
+    distribution_table = [[0] * (num_projects + 1) for _ in range(num_levels + 1)]
+
+    for project_index in range(1, num_projects+1):
+        for level_index in range(1, num_levels+1):
+            for prev_level in range (level_index+1):
+                current_level = level_index-prev_level
+                prev_profit = max_profit_table[prev_level][project_index-1]
+                current_profit = 0 if current_level == 0 else profit_matrix [current_level-1][project_index-1]
+                if (prev_profit+current_profit > max_profit_table[level_index][project_index]):
+                    max_profit_table[level_index][project_index] = prev_profit+current_profit
+                    distribution_table[level_index][project_index] = current_level
     distribution = [0] * num_projects
+    for project_index in range (num_projects,0,-1):
+        target_level = num_levels - sum(distribution)
+        distribution[project_index-1] = distribution_table[target_level][project_index]
 
-    for i in range(num_levels):
-        for j in range(num_projects):
-            if profit_matrix[i][j] < 0:
-                raise ProfitValueError(NEG_PROFIT_ERR_MSG, j, i)
-            if i > 0 and profit_matrix[i][j] < profit_matrix[i - 1][j]:
-                raise ProfitValueError(DECR_PROFIT_ERR_MSG, j, i)
-            if profit_matrix[i][j] > distribution[j]:
-                max_profit += profit_matrix[i][j] - distribution[j]
-                distribution[j] = profit_matrix[i][j]
-
-    return {PROFIT: max_profit, DISTRIBUTION: distribution}
+    return {PROFIT: max_profit_table [-1][-1], DISTRIBUTION:distribution }
 def main():
     profit_matrix = [[15, 18, 16, 17],
                      [20, 22, 23, 19],
