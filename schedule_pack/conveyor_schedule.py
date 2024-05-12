613b72132a05d4a853e555016c2a723b7a6985db
Дарья <dvchernova_1@edu.hse.ru>
2024-05-12 23:27:04 +0500

    Реализация оптимального расписания конвейнерой задачи по алгоритму Джонсона.

--- schedule_pack/conveyor_schedule.py
+++ schedule_pack/conveyor_schedule.py
@@ -55,13 +55,65 @@ class ConveyorSchedule(AbstractSchedule):
     def __fill_schedule(self, tasks: list[StagedTask]) -> None:
         """Процедура составляет расписание из элементов ScheduleItem для каждого
         исполнителя, согласно алгоритму Джонсона."""
-        pass
+        """Процедура составляет расписание из элементов ScheduleItem для каждого
+               исполнителя, согласно алгоритму Джонсона.
+
+               Parameters:
+               tasks (List[Task]): Список задач для расписания.
+
+               Returns:
+               List[List[ScheduleItem]]: Расписание для каждого исполнителя.
+               """
+        self._executor_schedule[0].append(
+            ScheduleItem(task=None, start=0, duration=tasks[0].stage_durations[0], is_downtime=True))
+        self._executor_schedule[1].append(ScheduleItem(task=tasks[0], start=0, duration=tasks[0].stage_durations[0]))
+
+        self._executor_schedule[0].append(
+            ScheduleItem(start=self._executor_schedule[1][0].end, duration=tasks[0].stage_durations[1], task=tasks[0]))
+
+        for task in tasks[1:]:
+            schedule_item_1 = ScheduleItem(start=self._executor_schedule[1][-1].end, duration=task.stage_durations[0],
+                                           task=task)
+            self._executor_schedule[1].append(schedule_item_1)
+            last_executor_schedule_1_end = self._executor_schedule[1][-1].end
+            last_executor_schedule_0_end = self._executor_schedule[0][-1].end
+
+            if last_executor_schedule_1_end > last_executor_schedule_0_end:
+                start = last_executor_schedule_1_end
+                duration = last_executor_schedule_1_end - last_executor_schedule_0_end
+            else:
+                start = last_executor_schedule_0_end
+                duration = last_executor_schedule_0_end - last_executor_schedule_1_end
+
+            schedule_item_0 = ScheduleItem(task=None, start=start, duration=duration, is_downtime=True)
+            self._executor_schedule[0].append(schedule_item_0)
+
+            schedule_item_2 = ScheduleItem(start=start, duration=task.stage_durations[1], task=task)
+            self._executor_schedule[0].append(schedule_item_2)
+
+        self._executor_schedule[1].append(ScheduleItem(task=None, start=self._executor_schedule[1][-1].end,
+                                                       duration=self._executor_schedule[0][-1].end -
+                                                                self._executor_schedule[1][-1].end,
+                                                       is_downtime=True))
+
+        self._executor_schedule[0], self._executor_schedule[1] = self._executor_schedule[1], self._executor_schedule[0]
 
     @staticmethod
     def __sort_tasks(tasks: list[StagedTask]) -> list[StagedTask]:
         """Возвращает отсортированный список задач для применения
         алгоритма Джонсона."""
-        pass
+        stage1_tasks: list[StagedTask] = []
+        stage2_tasks: list[StagedTask] = []
+
+        [stage1_tasks.append(task) if task.stage_duration(0) <= task.stage_duration(1) else stage2_tasks.append(task) for task in tasks]
+
+        stage1_tasks.sort(key=lambda task: task.stage_duration(0))
+        stage2_tasks.sort(key=lambda task: task.stage_duration(1), reverse=True)
+        print([stage.name for stage in stage1_tasks],[stage.name for stage in stage2_tasks])
+
+
+
+        return stage1_tasks + stage2_tasks
 
     @staticmethod
     def __validate_params(tasks: list[StagedTask]) -> None:
