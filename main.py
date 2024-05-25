303b45b57a4b4adeaa01856f2c382dcbeb01d516
yosheeeee <evantipin@edu.hse.ru>
2024-05-25 11:53:31 +0500

    done task

--- main.py
+++ main.py
@@ -19,7 +19,15 @@ def encode(numbers: list[int]) -> list[int]:
         :return: Альтернативная кодировка решения - список целых
         положительных чисел.
         """
-    pass
+    _check_encode_params(numbers)
+    result = []
+    while len(numbers) > 0:
+        sorted_numbers = sorted(numbers)
+        current_number = numbers[0]
+        current_number_index = sorted_numbers.index(current_number)
+        result.append(current_number_index + 1)
+        numbers.pop(0)
+    return result
 
 
 def decode(codes: list[int]) -> list[int]:
@@ -34,7 +42,51 @@ def decode(codes: list[int]) -> list[int]:
         :return: Натуральная кодировка решения - список целых
         положительных чисел.
         """
-    pass
+    _check_decode_params(codes)
+    numbers = [i+1 for i in range(len(codes))]
+    result  = []
+    for ind in codes:
+        current_number = numbers[ind-1]
+        result.append(current_number)
+        numbers.pop(ind -1)
+    return result
+
+
+
+
+def _check_decode_params(codes: list[int]):
+    _check_list_type(codes)
+    for i in range(len(codes)):
+        c = codes[i]
+        _check_param_type(c)
+        if (i == 0 and c != 1):
+            raise ValueError(ERR_NOT_START_WITH_1_MSG)
+        if (c > len(codes) - i):
+            raise ValueError(ERR_OVER_CONSTRAINT_TEMPL.format(c, i+1))
+
+
+def _check_param_type(code: int):
+    if (type(code) != int):
+        raise TypeError(ERR_NOT_INT_TEMPL.format(code))
+
+
+def _check_list_type(lst):
+    if (type(lst) != list):
+        raise TypeError(ERR_NOT_LIST_MSG)
+
+    if len(lst) == 0:
+        raise ValueError(ERR_EMPTY_LIST_MSG)
+
+
+def _check_encode_params(codes: list[int]):
+    _check_list_type(codes)
+    for i in range(len(codes)):
+        c = codes[i]
+        _check_param_type(c)
+        if (i == 0 and c != 1):
+            raise ValueError(ERR_NOT_START_WITH_1_MSG)
+        if codes.count(c) > 1:
+            raise ValueError(ERR_HAS_DUPLICATES_MSG)
 
 
 if __name__ == '__main__':
