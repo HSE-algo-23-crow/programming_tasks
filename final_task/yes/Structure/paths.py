1cba784e100962a2fa79cf89bf2fc576cfe015b0
ImRedTea <avbantikov@edu.hse.ru>
2024-06-02 21:10:10 +0500

    Не работает пока что

---
+++ final_task/yes/Structure/paths.py
@@ -0,0 +1,73 @@
+import random
+
+class Paths:
+    length = 0
+    names = []
+
+    __paths = list()
+    __names = dict()
+
+    __a = 1.0
+    __b = 1.0
+
+    def __init__(self, a, b):
+        self.__a = a
+        self.__b = b
+        pass
+
+    def add_point(self, name):
+        #check for bad data
+        pass
+
+        self.length += 1
+        for i in self.__names.values():
+            self.__paths[i].append([None, 1])
+        self.__paths.append([[None, 1],] * self.length)
+        self.__names[name] = self.length-1
+        self.names.append(name)
+
+    def add_path(self, name1, name2, dist):
+        #check for bad data
+        pass
+
+        point1 = self.__names[name1]
+        point2 = self.__names[name2]
+        self.__paths[point1][point2][0] = dist
+
+    def get_path(self, name1, name2):
+        point1 = self.__names[name1]
+        point2 = self.__names[name2]
+        return self.__paths[point1][point2][0]
+
+    def get_probability(self, name1, name2):
+        point1 = self.__names[name1]
+        point2 = self.__names[name2]
+        return self.__paths[point1][point2][1]**self.__a / self.__paths[point1][point2][0]**self.__b
+
+    def get_next_point(self, name1, visited):
+        points = list(set(self.__names.keys()).difference(set(visited)))
+        probabilities = [self.get_probability(name1, i) for i in points]
+        sm = sum(probabilities)
+        choice = random.random()*sm
+        stack = 0
+        for i in range(len(points)):
+            stack += probabilities[i]
+            if stack > choice:
+                return points[i]
+
+    def adjust_pheromones(self, name1, name2, adjustment):
+        point1 = self.__names[name1]
+        point2 = self.__names[name2]
+        self.__paths[point1][point2][1] += adjustment
+
+    def vaporize_pheromones(self, k):
+        for i in self.__paths:
+            for j in i:
+                j[1] *= k
+
+    def get_length(self, path):
+        length = 0
+        for i in range(len(path)-1):
+            length += self.get_path(path[i], path[i+1])
+        length += self.get_path(path[-1], path[0])
+        return length
--- final_task/yes/main.py
+++ final_task/yes/main.py
@@ -1,7 +1,35 @@
+from final_task.yes.Structure.paths import Paths
+from final_task.yes.Structure.ant import Ant
+
 # Здесь будет исходный код разработанного алгоритма
 
 def main():
-    print(f'Здесь будет пример использования разработанного алгоритма')
+    paths = Paths(1.0, 1.0)
+    N = int(input("Введите количество городов: "))
+    for i in range(N):
+        paths.add_point(input("Введите имя города #"+str(i+1)+": "))
+    for i in paths.names:
+        for j in paths.names:
+            if i != j:
+                paths.add_path(i,j, float(input("Введите расстояние между "+i+" и "+j+": ")))
+    ants = [Ant(paths) for i in range(N)]
+    best_path = [[], float("inf")]
+    for II in range(10):
+        ant_paths = [None for i in range(N)]
+        for i in range(N):
+            ants[i].set_position(paths.names[i])
+            ant_paths[i] = [ants[i].get_path(),0]
+            ant_paths[i][1] = paths.get_length(ant_paths[i][0])
+        for i in ant_paths:
+            if (best_path[1] > i[1]):
+                best_path = i
+        paths.vaporize_pheromones(0.9)
+        for i in ant_paths:
+            k = 10.0/i[1]
+            for j in range(len(i[0])-1):
+                paths.adjust_pheromones(i[0][j], i[0][j+1], k)
+            paths.adjust_pheromones(i[0][-1], i[0][0], k)
+        print(best_path)
 
 
 if __name__ == '__main__':
