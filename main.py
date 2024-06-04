366a6e20f98ef9d69bb8d6c21c0223b6e4dd3a27
rmzvch <remuzevich159@gmail.com>
2024-06-04 14:32:14 +0500

    that's it

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ ERR_HAS_DUPLICATES_MSG = 'Список содержит дубликаты'
 ERR_OVER_CONSTRAINT_TEMPL = ('Значение [{0}] в позиции [{1}] превышает '
                              'ограничение n - i + 1')
 
-
 def encode(numbers: list[int]) -> list[int]:
     """Переводит решение задачи коммивояжера из натуральной кодировки в
     альтернативную. Решение всегда начинается с единицы.
@@ -19,7 +18,34 @@ def encode(numbers: list[int]) -> list[int]:
         :return: Альтернативная кодировка решения - список целых
         положительных чисел.
         """
-    pass
+    if not (isinstance(numbers, list)):
+        raise TypeError(ERR_NOT_LIST_MSG)
+
+    if not (numbers):
+        raise ValueError(ERR_EMPTY_LIST_MSG)
+
+    if not (all(isinstance(item, int) for item in numbers)):
+        for item in numbers:
+            if not (isinstance(item, int)):
+                raise TypeError(ERR_NOT_INT_TEMPL.format(item))
+
+    if numbers[0] != 1:
+        raise ValueError(ERR_NOT_START_WITH_1_MSG)
+
+    if len(numbers) != len(set(numbers)):
+        raise ValueError(ERR_HAS_DUPLICATES_MSG)
+
+    encoded_arr = []
+    remaining_arr = []
+    for i in range(1, len(numbers) + 1):
+        remaining_arr.append(i)
+
+    for item in numbers:
+        index = remaining_arr.index(item)
+        encoded_arr.append(index + 1)
+        remaining_arr.pop(index)
+
+    return encoded_arr
 
 
 def decode(codes: list[int]) -> list[int]:
@@ -34,7 +60,33 @@ def decode(codes: list[int]) -> list[int]:
         :return: Натуральная кодировка решения - список целых
         положительных чисел.
         """
-    pass
+    if not (isinstance(codes, list)):
+        raise TypeError(ERR_NOT_LIST_MSG)
+
+    if not (codes):
+        raise ValueError(ERR_EMPTY_LIST_MSG)
+
+    if not (all(isinstance(code, int) for code in codes)):
+        for item in codes:
+            if not (isinstance(item, int)):
+                raise TypeError(ERR_NOT_INT_TEMPL.format(item))
+
+    if codes[0] != 1:
+        raise ValueError(ERR_NOT_START_WITH_1_MSG)
+
+    decoded_arr = []
+    remaining_arr = []
+    for i in range(1, len(codes) + 1):
+        remaining_arr.append(i)
+
+    for i, item in enumerate(codes):
+        if item > len(codes) - i:
+            raise ValueError(ERR_OVER_CONSTRAINT_TEMPL.format(item, i + 1))
+
+        decoded_arr.append(remaining_arr[item - 1])
+        remaining_arr.pop(item - 1)
+
+    return decoded_arr
 
 
 if __name__ == '__main__':
