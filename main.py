0e2d721ae6f8769e72831e23141c44b04c8bba41
rmzvch <remuzevich159@gmail.com>
2024-06-05 23:50:47 +0500

    Обновили в main.py & tests.py, что наша кодировка будет начинаться с нуля.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 ERR_NOT_LIST_MSG = 'Переданный параметр не является списком'
 ERR_EMPTY_LIST_MSG = 'Переданный список пуст'
 ERR_NOT_INT_TEMPL = 'Элемент списка [{0}] не является целым числом'
-ERR_NOT_START_WITH_1_MSG = 'Список не начинается с 1'
+ERR_NOT_START_WITH_0_MSG = 'Список не начинается с 0-го индекса'
 ERR_HAS_DUPLICATES_MSG = 'Список содержит дубликаты'
 ERR_OVER_CONSTRAINT_TEMPL = ('Значение [{0}] в позиции [{1}] превышает '
                              'ограничение n - i + 1')
@@ -29,20 +29,20 @@ def encode(numbers: list[int]) -> list[int]:
             if not (isinstance(item, int)):
                 raise TypeError(ERR_NOT_INT_TEMPL.format(item))
 
-    if numbers[0] != 1:
-        raise ValueError(ERR_NOT_START_WITH_1_MSG)
+    if numbers[0] != 0:
+        raise ValueError(ERR_NOT_START_WITH_0_MSG)
 
     if len(numbers) != len(set(numbers)):
         raise ValueError(ERR_HAS_DUPLICATES_MSG)
 
     encoded_arr = []
     remaining_arr = []
-    for i in range(1, len(numbers) + 1):
+    for i in range(0, len(numbers)):
         remaining_arr.append(i)
 
     for item in numbers:
         index = remaining_arr.index(item)
-        encoded_arr.append(index + 1)
+        encoded_arr.append(index)
         remaining_arr.pop(index)
 
     return encoded_arr
@@ -71,28 +71,27 @@ def decode(codes: list[int]) -> list[int]:
             if not (isinstance(item, int)):
                 raise TypeError(ERR_NOT_INT_TEMPL.format(item))
 
-    if codes[0] != 1:
-        raise ValueError(ERR_NOT_START_WITH_1_MSG)
+    if codes[0] != 0:
+        raise ValueError(ERR_NOT_START_WITH_0_MSG)
 
     decoded_arr = []
     remaining_arr = []
-    for i in range(1, len(codes) + 1):
+    for i in range(len(codes)):
         remaining_arr.append(i)
 
     for i, item in enumerate(codes):
         if item > len(codes) - i:
             raise ValueError(ERR_OVER_CONSTRAINT_TEMPL.format(item, i + 1))
-
-        decoded_arr.append(remaining_arr[item - 1])
-        remaining_arr.pop(item - 1)
+        decoded_arr.append(remaining_arr[item])
+        remaining_arr.pop(item)
 
     return decoded_arr
 
 
-if __name__ == '__main__':
+if name == 'main':
     print('Пример натуральной и альтернативной кодировки решения задачи о '
           'рюкзаке\n')
-    natural = [1, 5, 2, 4, 3]
-    alter = [1, 1, 1, 2, 1]
+    natural = [0, 4, 1, 3, 2]
+    alter = [0, 0, 0, 1, 0]
     print(f'Натуральный код {natural} -> Альтернативный код {encode(natural)}')
     print(f'Альтернативный код {alter} -> Натуральный код {decode(alter)}')
--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 import unittest
 
 from main import encode, decode, ERR_NOT_LIST_MSG, ERR_NOT_INT_TEMPL, \
-    ERR_EMPTY_LIST_MSG, ERR_NOT_START_WITH_1_MSG, ERR_HAS_DUPLICATES_MSG, \
+    ERR_EMPTY_LIST_MSG, ERR_NOT_START_WITH_0_MSG, ERR_HAS_DUPLICATES_MSG, \
     ERR_OVER_CONSTRAINT_TEMPL
 
 
@@ -17,8 +17,8 @@ class TestEncode(unittest.TestCase):
     def test_not_list_val(self):
         """Проверяет выброс исключения при передаче некорректного значения
         в списке."""
-        incorrect_lists = [[None], [1.1, 1], ['str', 1, 2],
-                           [{'key': 'val'}, 1]]
+        incorrect_lists = [[None], [0.1, 0], ['str', 0, 1],
+                           [{'key': 'val'}, 0]]
         for lst in incorrect_lists:
             with self.assertRaises(TypeError) as error:
                 encode(lst)
@@ -29,14 +29,14 @@ class TestEncode(unittest.TestCase):
         """Проверяет выброс исключения при передаче списка не начинающегося
         с единицы."""
         with self.assertRaises(ValueError) as error:
-            encode([2, 1, 3])
-        self.assertEqual(ERR_NOT_START_WITH_1_MSG, str(error.exception))
+            encode([1, 0, 2])
+        self.assertEqual(ERR_NOT_START_WITH_0_MSG, str(error.exception))
 
     def test_has_duplicates(self):
         """Проверяет выброс исключения при передаче списка содержащего
         дубликаты."""
         with self.assertRaises(ValueError) as error:
-            encode([1, 2, 3, 2])
+            encode([0, 1, 2, 1])
         self.assertEqual(ERR_HAS_DUPLICATES_MSG, str(error.exception))
 
     def test_empty_list(self):
@@ -47,34 +47,34 @@ class TestEncode(unittest.TestCase):
 
     def test_1(self):
         """Проверка кодировки 1 элемента."""
-        self.assertEqual([1], encode([1]))
+        self.assertEqual([0], encode([0]))
 
     def test_2(self):
         """Проверка кодировки 2 элементов."""
-        self.assertEqual([1, 1], encode([1, 2]))
+        self.assertEqual([0, 0], encode([0, 1]))
 
     def test_3(self):
         """Проверка кодировки 3 элементов."""
-        self.assertEqual([1, 2, 1], encode([1, 3, 2]))
+        self.assertEqual([0, 1, 0], encode([0, 2, 1]))
 
     def test_4(self):
         """Проверка кодировки 4 элементов."""
-        self.assertEqual([1, 2, 1, 1], encode([1, 3, 2, 4]))
+        self.assertEqual([0, 1, 0, 0], encode([0, 2, 1, 3]))
 
     def test_5_asc(self):
         """Проверка кодировки 5 элементов, отсортированных по возрастанию."""
-        self.assertEqual([1, 1, 1, 1, 1], encode([1, 2, 3, 4, 5]))
+        self.assertEqual([0, 0, 0, 0, 0], encode([0, 1, 2, 3, 4]))
 
     def test_6_desc(self):
         """Проверка кодировки 6 элементов, отсортированных по убыванию."""
-        self.assertEqual([1, 5, 4, 3, 2, 1], encode([1, 6, 5, 4, 3, 2]))
+        self.assertEqual([0, 4, 3, 2, 1, 0], encode([0, 5, 4, 3, 2, 1]))
 
     def test_20(self):
         """Проверка кодировки 20 элементов."""
-        natural = [1, 18, 5, 19, 7, 16, 17, 12, 10, 13, 14, 11, 9, 15, 6, 4,
-                   20, 8, 2, 3]
-        alter = [1, 17, 4, 16, 5, 13, 13, 9, 7, 8, 8, 7, 6, 6, 4, 3, 4, 3, 1,
-                 1]
+        natural = [0, 17, 4, 18, 6, 15, 16, 11, 9, 12, 13, 10, 8, 14, 5, 3,
+                   19, 7, 1, 2]
+        alter = [0, 16, 3, 15, 4, 12, 12, 8, 6, 7, 7, 6, 5, 5, 3, 2, 3, 2, 0,
+                 0]
         self.assertEqual(alter, encode(natural))
 
 
@@ -90,65 +90,64 @@ class TestDecode(unittest.TestCase):
     def test_not_list_val(self):
         """Проверяет выброс исключения при передаче некорректного значения
         в списке."""
-        incorrect_lists = [[None], [1.1, 1], ['str', 1, 2],
-                           [{'key': 'val'}, 1]]
+        incorrect_lists = [[None], [0.1, 0], ['str', 0, 1],
+                           [{'key': 'val'}, 0]]
         for lst in incorrect_lists:
             with self.assertRaises(TypeError) as error:
                 decode(lst)
             self.assertEqual(ERR_NOT_INT_TEMPL.format(lst[0]),
                              str(error.exception))
 
-    def test_not_start_with_1(self):
-        """Проверяет выброс исключения при передаче списка не начинающегося
-        с единицы."""
-        with self.assertRaises(ValueError) as error:
-            decode([2, 1, 3])
-        self.assertEqual(ERR_NOT_START_WITH_1_MSG, str(error.exception))
+        def test_not_start_with_1(self):
+            """Проверяет выброс исключения при передаче списка не начинающегося
+            с единицы."""
+            with self.assertRaises(ValueError) as error:
+                decode([1, 0, 2])
+            self.assertEqual(ERR_NOT_START_WITH_0_MSG, str(error.exception))
 
-    def test_empty_list(self):
-        """Проверяет выброс исключения при передаче пустого списка."""
-        with self.assertRaises(ValueError) as error:
-            decode([])
-        self.assertEqual(ERR_EMPTY_LIST_MSG, str(error.exception))
+        def test_empty_list(self):
+            """Проверяет выброс исключения при передаче пустого списка."""
+            with self.assertRaises(ValueError) as error:
+                decode([])
+            self.assertEqual(ERR_EMPTY_LIST_MSG, str(error.exception))
 
-    def test_over_constraint(self):
-        """Проверяет выброс исключения при передаче пустого списка."""
-        with self.assertRaises(ValueError) as error:
-            decode([1, 2])
-        self.assertEqual(ERR_OVER_CONSTRAINT_TEMPL.format(2, 2), str(error.exception))
+        def test_over_constraint(self):
+            """Проверяет выброс исключения при передаче пустого списка."""
+            with self.assertRaises(ValueError) as error:
+                decode([0, 1])
+            self.assertEqual(ERR_OVER_CONSTRAINT_TEMPL.format(1, 1), str(error.exception))
 
-    def test_1(self):
-        """Проверка декодирования 1 элемента."""
-        self.assertEqual([1], decode([1]))
+        def test_1(self):
+            """Проверка декодирования 1 элемента."""
+            self.assertEqual([0], decode([0]))
 
-    def test_2(self):
-        """Проверка декодирования 2 элементов."""
-        self.assertEqual([1, 2], decode([1, 1]))
+        def test_2(self):
+            """Проверка декодирования 2 элементов."""
+            self.assertEqual([0, 1], decode([0, 0]))
 
-    def test_3(self):
-        """Проверка декодирования 3 элементов."""
-        self.assertEqual([1, 3, 2], decode([1, 2, 1]))
+        def test_3(self):
+            """Проверка декодирования 3 элементов."""
+            self.assertEqual([0, 2, 1], decode([0, 1, 0]))
 
-    def test_4(self):
-        """Проверка декодирования 4 элементов."""
-        self.assertEqual([1, 3, 2, 4], decode([1, 2, 1, 1]))
+        def test_4(self):
+            """Проверка декодирования 4 элементов."""
+            self.assertEqual([0, 2, 1, 3], decode([0, 1, 0, 0]))
 
-    def test_5_asc(self):
-        """Проверка декодирования 5 элементов, отсортированных по возрастанию."""
-        self.assertEqual([1, 2, 3, 4, 5], decode([1, 1, 1, 1, 1]))
+        def test_5_asc(self):
+            """Проверка декодирования 5 элементов, отсортированных по возрастанию."""
+            self.assertEqual([0, 1, 2, 3, 4], decode([0, 0, 0, 0, 0]))
 
-    def test_6_desc(self):
-        """Проверка декодирования 6 элементов, отсортированных по убыванию."""
-        self.assertEqual([1, 6, 5, 4, 3, 2], decode([1, 5, 4, 3, 2, 1]))
+        def test_6_desc(self):
+            """Проверка декодирования 6 элементов, отсортированных по убыванию."""
+            self.assertEqual([0, 5, 4, 3, 2, 1], decode([0, 4, 3, 2, 1, 0]))
 
-    def test_20(self):
-        """Проверка декодирования 20 элементов."""
-        natural = [1, 18, 5, 19, 7, 16, 17, 12, 10, 13, 14, 11, 9, 15, 6, 4,
-                   20, 8, 2, 3]
-        alter = [1, 17, 4, 16, 5, 13, 13, 9, 7, 8, 8, 7, 6, 6, 4, 3, 4, 3, 1,
-                 1]
-        self.assertEqual(natural, decode(alter))
+        def test_20(self):
+            """Проверка декодирования 20 элементов."""
+            natural = [0, 17, 4, 18, 6, 15, 16, 11, 9, 12, 13, 10, 8, 14, 5, 3,
+                       19, 7, 1, 2]
+            alter = [0, 16, 3, 15, 4, 12, 12, 8, 6, 7, 7, 6, 5, 5, 3, 2, 3, 2, 0,
+                     0]
+            self.assertEqual(natural, decode(alter))
 
-
-if __name__ == '__main__':
-    unittest.main()
+    if name == 'main':
+        unittest.main()
