9095ce1611d71c8c1af1734ab44f7ed8f3600348
Михаил Катан <zakcse325@gmail.com>
2024-06-07 04:03:00 +0500

    Валидацию начал

--- main.py
+++ main.py
@@ -6,7 +6,31 @@ ERR_HAS_DUPLICATES_MSG = 'Список содержит дубликаты'
 ERR_OVER_CONSTRAINT_TEMPL = ('Значение [{0}] в позиции [{1}] превышает '
                              'ограничение n - i + 1')
 
+def decode_validation(numbers: list[int]):
+    if not isinstance(numbers, list):
+        raise TypeError(ERR_NOT_LIST_MSG)
+    elif len(numbers) == 0:
+        raise ValueError(ERR_EMPTY_LIST_MSG)
+    elif numbers[0] != 1:
+        raise ValueError(ERR_NOT_START_WITH_1_MSG)
+    else:
+        for item in numbers:
+            if not isinstance(item, int):
+                raise TypeError(ERR_NOT_INT_TEMPL)
 
+def encode_validation(numbers: list[int]):
+    if not isinstance(numbers, list):
+        raise TypeError(ERR_NOT_LIST_MSG)
+    elif len(numbers) == 0:
+        raise ValueError(ERR_EMPTY_LIST_MSG)
+    elif numbers[0] != 1:
+        raise ValueError(ERR_NOT_START_WITH_1_MSG)
+    elif len(numbers) != len(set(numbers)):
+        raise ValueError(ERR_HAS_DUPLICATES_MSG)
+    else:
+        for item in numbers:
+            if not isinstance(item, int):
+                raise TypeError(ERR_NOT_INT_TEMPL)
 def encode(numbers: list[int]) -> list[int]:
     """Переводит решение задачи коммивояжера из натуральной кодировки в
     альтернативную. Решение всегда начинается с единицы.
@@ -19,7 +43,9 @@ def encode(numbers: list[int]) -> list[int]:
         :return: Альтернативная кодировка решения - список целых
         положительных чисел.
         """
-    pass
+    encode_validation(numbers)
+
+    return numbers
 
 
 def decode(codes: list[int]) -> list[int]:
@@ -34,13 +60,14 @@ def decode(codes: list[int]) -> list[int]:
         :return: Натуральная кодировка решения - список целых
         положительных чисел.
         """
-    pass
+    decode_validation(codes)
+    return codes
 
 
 if __name__ == '__main__':
     print('Пример натуральной и альтернативной кодировки решения задачи о '
           'рюкзаке\n')
     natural = [1, 5, 2, 4, 3]
-    alter = [1, 1, 1, 2, 1]
+    alter = None
     print(f'Натуральный код {natural} -> Альтернативный код {encode(natural)}')
     print(f'Альтернативный код {alter} -> Натуральный код {decode(alter)}')
