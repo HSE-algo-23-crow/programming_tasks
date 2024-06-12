e2d3fee9933c4411283fed7c24d25ae5aaca0299
Михаил Катан <zakcse325@gmail.com>
2024-06-13 03:24:23 +0500

    Не проходят тесты для 1 задания, возможно я не правильно понял, что нам нужно сделать, но пока уверен, что тесты показывают что-то не то

--- main.py
+++ main.py
@@ -9,9 +9,15 @@ def get_win_sequence(input_string: str) -> str:
     :return: Строку, представляющую минимальную лексикографическую
     подпоследовательность исходной строки.
     """
-    pass
-
-
+    line = list(input_string.split("\n"))
+    n = int(line[0])
+    string = line[1]
+    min_letter = min(string)
+    sequences = []
+    for i in range(len(string)):
+        if string[i] == min_letter:
+            sequences.append(string[i:])
+    return min(sequences)
 def get_water_volume(input_string: str) -> int:
     """Вычисляет объем воды (количество блоков), который остается после дождя
     на острове.
@@ -22,15 +28,31 @@ def get_water_volume(input_string: str) -> int:
     :return: Целое число, представляющее объем воды (в блоках), который
     останется после дождя.
     """
-    pass
+    input_string = list(input_string.split("\n"))
+    n = int(input_string[0])
+    heights = list(map(int, str(input_string[1]).split()))
+    left_maxes = [0] * n
+    right_maxes = [0] * n
+
+    left_maxes[0] = heights[0]
+    for i in range(1, n):
+        left_maxes[i] = max(left_maxes[i - 1], heights[i])  # ищем все максимумы слева и записываем в список
+    right_maxes[n - 1] = heights[n - 1]
+    for i in range(n - 2, -1, -1):
+        right_maxes[i] = max(right_maxes[i + 1], heights[i])  # ищем все максимумы справа и записываем в список
+    count = 0
+    for i in range(n):
+        count += min(left_maxes[i], right_maxes[i]) - heights[
+            i]  # исчитаем блоки с водой, берем мин высоту правого и левого максимума для данной вершины и вычитаем ее высоту
+    return count
 
 
 def main():
     print(get_water_volume('11\n2 5 2 3 6 9 3 1 3 4 6'))  # 18
 
     print(get_win_sequence('4\nMAMA'))  # A
-    print(get_win_sequence('4\nALLOALLO'))  # ALLO
-    print(get_win_sequence('6\nABCOAXLO'))  # ABCOAX
+    print(get_win_sequence('8\nALLOALLO'))  # ALLO
+    print(get_win_sequence('21\nASZYAFAXZEWWAVQHJHBPELYZP'))  # ABCOAX
 
 
 if __name__ == '__main__':
