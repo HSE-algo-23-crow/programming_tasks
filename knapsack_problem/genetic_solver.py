e9c393fa39c799dbe824e97e2fa1c5b8d7a62010
Enotta <nvageev_1@edu.hse.ru>
2024-06-17 12:06:47 +0500

    Алгоритм готов, остальным соболезную

--- knapsack_problem/genetic_solver.py
+++ knapsack_problem/genetic_solver.py
@@ -4,6 +4,8 @@ from knapsack_problem.constants import COST, ITEMS, POPULATION_LIMIT, EPOCH_CNT,
     BRUTE_FORCE_BOUND
 from knapsack_problem.brute_force import brute_force
 from knapsack_problem.validate import validate_params
+import itertools
+from random import choice, randint
 
 
 class GeneticSolver:
@@ -36,12 +38,102 @@ class GeneticSolver:
         self.__population_cnt = min(2 ** self.__item_cnt / 2, POPULATION_LIMIT)
         self.__population = self.__generate_population(self.__population_cnt)
 
-    @property
-    def population(self) -> list[tuple[str, int]]:
-        """Возвращает список особей текущей популяции. Для каждой особи
-        возвращается строка из 0 и 1, а также значение финтес-функции.
-        """
-        pass
+    @staticmethod
+    def __generate_combination(self, combination_length: int) -> str:
+        res = ''
+        for i in range(combination_length):
+            res += choice(['0', '1'])
+        return res
+
+    def __comb_weight(self, c: str) -> int:
+        weight = 0
+        for i in range(len(c)):
+            if c[i] == '1': weight += self.__weights[i]
+
+        return weight
+
+    def __comb_fitness(self, c: str) -> int:
+        fitness = 0
+        for i in range(len(c)):
+            if c[i] == '1': fitness += self.__costs[i]
+
+        return fitness
+
+    def __generate_population(self, population_cnt: int) -> list[tuple[str, int]]:
+        result = []
+        while len(result) != self.__population_cnt:
+            comb = self.__generate_combination(self, self.__item_cnt)
+            comb_weight = self.__comb_weight(comb)
+            if comb_weight <= self.__weight_limit:
+                result.append((comb, self.__comb_fitness(comb)))
+
+        return result
+
+    def __roulette(self, common_len: int) -> int:
+        ch = randint(0, common_len)
+
+        i = 0
+        while True:
+            if ch - self.__population[i][1] > 0:
+                ch -= self.__population[i][1]
+                i += 1
+            else:
+                break
+
+        return i
+
+    def __get_crossing(self) -> list[int]:
+        result = []
+
+        cl = 0
+        for i in self.__population:
+            cl += i[1]
+
+        while len(result) != self.__population_cnt // 4:
+            new = self.__roulette(cl)
+            if not (new in result):
+                result.append(new)
+
+        return result
+
+    def __mutation(self, c: str):
+        while self.__comb_weight(c) > self.__weight_limit:
+            ones_indices = [i for i, char in enumerate(c) if char == '1']
+            random_index = choice(ones_indices)
+
+            c = list(c)
+            c[random_index] = '0'
+            c = ''.join(c)
+
+        return c
+
+    def __cross_two(self, u1: tuple[str, int], u2: tuple[str, int]):
+        barrier = randint(1, len(u1[0])-1)
+
+        new_unit1 = u1[0][:barrier]+u2[0][barrier:]
+        new_unit2 = u2[0][:barrier]+u1[0][barrier:]
+
+        if self.__comb_weight(new_unit1) <= self.__weight_limit:
+            self.__population.append((new_unit1, self.__comb_fitness(new_unit1)))
+        else:
+            new_unit1 = self.__mutation(new_unit1)
+            self.__population.append((new_unit1, self.__comb_fitness(new_unit1)))
+
+        if self.__comb_weight(new_unit2) <= self.__weight_limit:
+            self.__population.append((new_unit2, self.__comb_fitness(new_unit2)))
+        else:
+            new_unit2 = self.__mutation(new_unit2)
+            self.__population.append((new_unit2, self.__comb_fitness(new_unit2)))
+
+    def __cross_population(self, crs: list[int]):
+        while len(crs) != 0:
+            unit1 = self.__population[crs.pop(0)]
+            unit2 = self.__population[crs.pop(0)]
+
+            self.__cross_two(unit1, unit2)
+
+    def __cut_population(self):
+        self.__population = self.__population[:int(self.__population_cnt)]
 
     def get_knapsack(self, epoch_cnt=EPOCH_CNT) -> dict[str, int | list[int]]:
         """Запускает генетический алгоритм для решения задачи о рюкзаке.
@@ -51,7 +143,21 @@ class GeneticSolver:
         рюкзаке, items - список с индексами предметов, обеспечивающих максимальную
         стоимость.
         """
-        pass
+        for _ in range(epoch_cnt):
+            self.__population = sorted(self.__population, key=lambda x: -x[1])
+            crossing = self.__get_crossing()
+            self.__cross_population(crossing)
+            self.__population = sorted(self.__population, key=lambda x: -x[1])
+            self.__cut_population()
+
+        return self.__population[0]
+
+    @property
+    def population(self) -> list[tuple[str, int]]:
+        """Возвращает список особей текущей популяции. Для каждой особи
+        возвращается строка из 0 и 1, а также значение финтес-функции.
+        """
+        return self.__population
 
 
 if __name__ == '__main__':
---
+++ test/my_test.py
@@ -0,0 +1,2 @@
+a = 'aasasas'
+print(list(a))
