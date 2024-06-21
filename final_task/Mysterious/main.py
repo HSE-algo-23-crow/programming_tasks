4ee419e5e51d48f261703ffe06fea8345103b8b8
Myron Butakov <mabutakov_1@edu.hse.ru>
2024-06-21 18:14:36 +0500

    1) Сформулированы критерии тестирования - что нужно проверить для того, чтобы убедиться в правильности работы алгоритма 2) Разработан набор тестов согласно подготовленным критериям. 3) Реализован алгоритм, успешно проходящий подготовленные тесты.

--- final_task/Mysterious/main.py
+++ final_task/Mysterious/main.py
@@ -1,7 +1,99 @@
-# Здесь будет исходный код разработанного алгоритма
+ERR_EMPTY_BOTH_STRINGS = "Начальная строка и искомая подстрока являются пустыми."
+ERR_EMPTY_INIT_STRING = "Начальная строка является пустой."
+ERR_EMPTY_SUBSTRING = "Искомая подстрока является пустой."
+ERR_SUBSTRING_LARGE_THAN_INIT_STRING = "Искомая подстрока больше начальной подстроки."
+
+
+def __get_valid_params(substring: str, init_string: str):
+    """Валидация параметров.
+
+    :param substring: Искомая подстрока.
+    :param init_string: Начальная строка, в которой будет осуществляться поиск подстроки.
+    :raise ValueError: Если искомая подстрока или начальная строка является пустой,
+     или если подстрока больше начальной строки
+    """
+
+    if len(init_string) == 0 and len(substring) == 0:
+        raise ValueError(ERR_EMPTY_BOTH_STRINGS)
+
+    if len(init_string) == 0:
+        raise ValueError(ERR_EMPTY_INIT_STRING)
+
+    if len(substring) == 0:
+        raise ValueError(ERR_EMPTY_SUBSTRING)
+
+    if len(substring) > len(init_string):
+        raise ValueError(ERR_SUBSTRING_LARGE_THAN_INIT_STRING)
+
+
+def substring_search(substring: str, init_string: str) -> int:
+    """Алгоритм Бойера-Мура-Хорспула: оптимальный поиск подстроки в строке.
+
+    :param substring: Искомая подстрока.
+    :param init_string: Начальная строка, в которой будет осуществляться поиск подстроки.
+    :return: Индекс начала первого вхождения подстроки или -1, если подстрока не найдена.
+    """
+
+    # Валидация параметров
+    __get_valid_params(substring, init_string)
+
+    # Формирование таблицы смещений
+    uniq_symbols = set()
+    len_sub = len(substring)
+    offsets = {}
+
+    for i in range(len_sub - 2, -1, -1):
+        if substring[i] not in uniq_symbols:
+            offsets[substring[i]] = len_sub - i - 1
+            uniq_symbols.add(substring[i])
+
+    if substring[len_sub - 1] not in uniq_symbols:
+        offsets[substring[len_sub - 1]] = len_sub
+
+    offsets["*"] = len_sub
+
+    # Поиск подстроки в строке
+    len_init = len(init_string)
+    i = len_sub - 1
+
+    while i < len_init:
+        k = 0
+        flag = False
+
+        for j in range(len_sub - 1, -1, -1):
+            if init_string[i - k] != substring[j]:
+                if j == len_sub - 1:
+                    off = offsets[init_string[i]] if offsets.get(init_string[i], False) else offsets["*"]
+                else:
+                    off = offsets[substring[j]]
+
+                i += off
+                flag = True
+                break
+
+            k += 1
+
+        if not flag:
+            return i - k + 1
+
+    else:
+        return -1
+
 
 def main():
-    print(f'Здесь будет пример использования разработанного алгоритма')
+    init_string = "Сын «пятого битла» тоже продюсировал музыку The Beatles."
+    substring = "тоже"
+
+    index = substring_search(substring, init_string)
+
+    print("Пример поиска подстроки в строке алгоритмом Бойера-Мура-Хорспула\n")
+    print(f"Начальная строка: {init_string}")
+    print(f"Искомая подстрока: {substring}\n")
+
+    if not(index == -1):
+        print(f"Искомая подстрока найдена по индексу: {index}")
+    else:
+        print(f"Данная подстрока отсутствует в начальной строке")
 
 
 if __name__ == '__main__':
--- final_task/Mysterious/test.py
+++ final_task/Mysterious/test.py
@@ -1,12 +1,69 @@
 import unittest
+from main import (substring_search, ERR_EMPTY_BOTH_STRINGS, ERR_EMPTY_INIT_STRING, ERR_EMPTY_SUBSTRING,
+                  ERR_SUBSTRING_LARGE_THAN_INIT_STRING)
 
 
-class Test(unittest.TestCase):
-    """Это будет класс для тестирования разработанного алгоритма"""
+class TestSubstringSearch(unittest.TestCase):
 
-    def test_something(self):
-        """Здесь будут тест кейсы тестирования разработанного алгоритма"""
-        self.assertEqual(1, 1)
+    def test_1_empty_both_strings(self):
+        """Проверяет выброс уникального исключения при пустых начальной строке и подстроке"""
+        with self.assertRaises(ValueError) as error:
+            substring_search("", "")
+        self.assertEqual(ERR_EMPTY_BOTH_STRINGS, str(error.exception))
+
+    def test_2_empty_substring(self):
+        """Проверяет выброс исключения при пустой подстроке"""
+        with self.assertRaises(ValueError) as error:
+            substring_search("", "Бла-бла")
+        self.assertEqual(ERR_EMPTY_SUBSTRING, str(error.exception))
+
+    def test_3_empty_init_string(self):
+        """Проверяет выброс исключения при пустой начальной строке"""
+        with self.assertRaises(ValueError) as error:
+            substring_search("Бла", "")
+        self.assertEqual(ERR_EMPTY_INIT_STRING, str(error.exception))
+
+    def test_4_substring_large_than_init_string(self):
+        """Проверяет выброс исключения, если длина искомой подстроки больше, чем длина начальной строки"""
+        with self.assertRaises(ValueError) as error:
+            substring_search("Я длиннее начальной строки", "Я - начальная строка")
+        self.assertEqual(ERR_SUBSTRING_LARGE_THAN_INIT_STRING, str(error.exception))
+
+    def test_5(self):
+        """Проверка поиска при отсутствии искомой подстроки в начальной строке"""
+        init_string = "Первая в Европе женщина-офтальмолог родилась в Черновцах."
+        substring = "мужчина"
+        index = substring_search(substring, init_string)
+        self.assertEqual(index, -1)
+
+    def test_6(self):
+        """Проверка поиска при отсутствии искомой подстроки в начальной строке"""
+        init_string = "Одной из лучших игр эпохи 16-битных консолей считается платформер, рекламирующий McDonald’s."
+        substring = "Burger King"
+        index = substring_search(substring, init_string)
+        self.assertEqual(index, -1)
+
+    def test_7(self):
+        """Проверка поиска присутствующей подстроки в строке"""
+        init_string = "Первая в Европе женщина-офтальмолог родилась в Черновцах."
+        substring = "женщина"
+        index = substring_search(substring, init_string)
+        self.assertEqual(index, 16)
+
+    def test_8(self):
+        """Проверка поиска присутствующей подстроки в строке"""
+        init_string = "Одной из лучших игр эпохи 16-битных консолей считается платформер, рекламирующий McDonald’s."
+        substring = "лучших игр"
+        index = substring_search(substring, init_string)
+        self.assertEqual(index, 9)
+
+    def test_9(self):
+        """Проверка поиска присутствующей подстроки в строке"""
+        init_string = ("История штата Нью-Йорк началась со времени заселения территории штата человеком примерно "
+                       "в 10 000 году до н. э.")
+        substring = "Нью-Йорк"
+        index = substring_search(substring, init_string)
+        self.assertEqual(index, 14)
 
 
 if __name__ == '__main__':
