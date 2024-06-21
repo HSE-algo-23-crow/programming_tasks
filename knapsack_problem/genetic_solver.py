f38b2a6f706fa66fcca5e311d41ac96660650515
Yaroslav Evdokimov <113132061+YRGBTW@users.noreply.github.com>
2024-06-21 19:13:11 +0500

    Update genetic_solver.py
    Попробовал исправить, но один тест почему-то никак не дается :(

--- knapsack_problem/genetic_solver.py
+++ knapsack_problem/genetic_solver.py
@@ -2,19 +2,20 @@ import random as rnd
 from knapsack_problem.constants import COST, ITEMS, POPULATION_LIMIT, EPOCH_CNT, BRUTE_FORCE_BOUND
 from knapsack_problem.brute_force import brute_force
 from knapsack_problem.validate import validate_params
+from functools import lru_cache
 
 class GeneticSolver:
     """Класс для решения задачи о рюкзаке с использованием генетического
     алгоритма. Для входных данных небольшого размера используется полный
     перебор.
-    
+
     Экземпляр класса хранит состояние популяции, метод поиска решения может
     быть запущен многократно для одного экземпляра.
     """
 
     def __init__(self, weights: list[int], costs: list[int], weight_limit: int):
         """Создает объект класса для решения задачи о рюкзаке.
-        
+
         :param weights: Список весов предметов для рюкзака.
         :param costs: Список стоимостей предметов для рюкзака.
         :param weight_limit: Ограничение вместимости рюкзака.
@@ -39,20 +40,19 @@ class GeneticSolver:
             population.add(individual)
         return [list(ind) for ind in population]
 
+    @lru_cache(None)
     def fitness(self, individual):
-        total_weight = sum(self.__weights[i] for i in range(self.__item_cnt) if individual[i] == 1)
-        total_value = sum(self.__costs[i] for i in range(self.__item_cnt) if individual[i] == 1)
-        if total_weight > self.__weight_limit:
-            return 0
-        else:
-            return total_value
+        total_weight = sum(w for w, i in zip(self.__weights, individual) if i)
+        total_value = sum(c for c, i in zip(self.__costs, individual) if i)
+        return total_value if total_weight <= self.__weight_limit else 0
 
-    def get_two_best(self, population):
-        non_zero_population = [ind for ind in population if self.fitness(ind) > 0]
-        if len(non_zero_population) < 2:
-            return rnd.sample(population, 2)
-        weights = [self.fitness(ind) for ind in non_zero_population]
-        return rnd.choices(non_zero_population, weights=weights, k=2)
+    def select_parents(self, population):
+        non_zero_population = [ind for ind in population if self.fitness(tuple(ind)) > 0]
+        if len(non_zero_population) < self.__population_cnt:
+            non_zero_population.extend(rnd.choices(population, k=self.__population_cnt - len(non_zero_population)))
+        weights = [self.fitness(tuple(ind)) for ind in non_zero_population]
+        parents = rnd.choices(non_zero_population, weights=weights, k=self.__population_cnt)
+        return parents
 
     def crossover(self, parent1, parent2):
         point = rnd.randint(1, self.__item_cnt - 1)
@@ -64,40 +64,42 @@ class GeneticSolver:
             individual[point] = 1 - individual[point]
         return individual
 
-    def genetic_algorithm(self):
+    def genetic_algorithm(self, epoch_cnt):
         population = self.__population
-        for _ in range(EPOCH_CNT):
+        for _ in range(epoch_cnt):
+            parents = self.select_parents(population)
             new_population = set()
-            while len(new_population) < self.__population_cnt:
-                parent1, parent2 = self.get_two_best(population)
+            for i in range(0, len(parents), 2):
+                parent1 = parents[i]
+                parent2 = parents[i + 1] if i + 1 < len(parents) else parents[0]
                 child1, child2 = self.crossover(parent1, parent2)
                 new_population.add(tuple(self.mutation(child1)))
                 if len(new_population) < self.__population_cnt:
                     new_population.add(tuple(self.mutation(child2)))
             population = [list(ind) for ind in new_population]
-        return max(population, key=self.fitness)
+        self.__population = population
+        return max(population, key=lambda ind: self.fitness(tuple(ind)))
 
     @property
     def population(self):
         """Возвращает список особей текущей популяции. Для каждой особи
         возвращается строка из 0 и 1, а также значение финтес-функции.
         """
-        return [(self.__mask.format(int("".join(map(str, ind)), 2)), self.fitness(ind)) for ind in self.__population]
+        return [(self.__mask.format(int("".join(map(str, ind)), 2)), self.fitness(tuple(ind))) for ind in self.__population]
 
     def get_knapsack(self, epoch_cnt=EPOCH_CNT):
         """Запускает генетический алгоритм для решения задачи о рюкзаке.
         Алгоритм выполняется заданное количество поколений.
-        
+
         :return: Словарь с ключами: cost - максимальная стоимость предметов в
         рюкзаке, items - список с индексами предметов, обеспечивающих максимальную
         стоимость.
         """
-        global EPOCH_CNT
-        EPOCH_CNT = epoch_cnt
-        best_individual = self.genetic_algorithm()
-        best_cost = self.fitness(best_individual)
+        best_individual = self.genetic_algorithm(epoch_cnt)
+        best_cost = self.fitness(tuple(best_individual))
         best_items = [i for i, gene in enumerate(best_individual) if gene == 1]
         return {'cost': best_cost, 'items': best_items}
+
 
 if __name__ == '__main__':
     weights = [67, 30, 8, 50, 94, 24, 3, 78]
